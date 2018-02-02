e93f329652ee24f99fecb7c7dfbb9668f7612d61
Erik Wiklander <erik.wiklander@uc.se>
2018-02-02 12:24:22 +0100

    Latest version of Spring Boot and added cors and csrf

--- src/main/java/com/wiklandia/ui/security/SecurityConfig.java
+++ src/main/java/com/wiklandia/ui/security/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.wiklandia.ui.security;
 
 import org.springframework.boot.autoconfigure.security.Http401AuthenticationEntryPoint;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.AllArgsConstructor;
 
@@ -16,25 +21,35 @@ import lombok.AllArgsConstructor;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private final SecurityProperties securityProperties;
+    private final SecurityProperties securityProperties;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/api/**").authenticated();
-		http.authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/**").authenticated();
+        http.authorizeRequests().antMatchers("/**").permitAll();
 
-		http.csrf().disable();
+        http.cors();
 
-		http.logout().logoutSuccessUrl("/logoutSuccess");
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 
-		http.httpBasic().authenticationEntryPoint(new Http401AuthenticationEntryPoint("Wiklandia"));
-		http.formLogin().disable();
+        http.logout().logoutSuccessUrl("/logoutSuccess");
 
-		if (securityProperties.isRequireSsl()) {
-			http.requiresChannel().anyRequest().requiresSecure();
-		}
+        http.httpBasic().authenticationEntryPoint(new Http401AuthenticationEntryPoint("Wiklandia"));
+        http.formLogin().disable();
 
-	}
+        if (securityProperties.isRequireSsl()) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        }
+
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
