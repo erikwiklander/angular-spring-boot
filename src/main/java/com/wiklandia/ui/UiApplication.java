c916b193a461febe21a10f211f5bface6d5e7f1d
erikwiklander <erik.wiklander@wiklandia.com>
2018-03-12 21:25:22 +0100

    Simplified redirect

--- src/main/java/com/wiklandia/ui/UiApplication.java
+++ src/main/java/com/wiklandia/ui/UiApplication.java
@@ -3,13 +3,21 @@ package com.wiklandia.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @EnableJpaAuditing
 @SpringBootApplication
 public class UiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@GetMapping(value = "/{path:[^\\.]*}")
+	public String redirect() {
+		return "forward:/";
 	}
 
 }
